3b643c5ddf4a2af1e92be8802592f877dbcfc80d
sathish <2000sathishkumar.d@gmail.com>
2023-01-02 17:44:20 +0530

    small changes in my code

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const btnOpen = document.getElementById("btn-open");
 const modalEl = document.getElementById("modal");
 const btnClose = document.getElementById("modal-close");
 
+// function
 function closeModal() {
   modalEl.classList.remove("close");
   modalEl.classList.add("show");
@@ -13,15 +14,16 @@ function showModal() {
 }
 
 btnOpen.addEventListener("click", function () {
-  showModal();
+  closeModal();
 });
 
 btnClose.addEventListener("click", function () {
-  closeModal();
+  showModal();
 });
 
 window.addEventListener("keydown", function (event) {
   if (event.key === "Escape") {
-    closeModal();
+    modalEl.classList.add("close");
+    modalEl.classList.remove("show");
   }
 });
